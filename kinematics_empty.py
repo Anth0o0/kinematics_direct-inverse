05d0372d548878312ba310fbe2599599860dd027
Anth0o0 <anthony.raffeneau@ynov.com>
2020-10-08 01:56:21 +0200

    TP1+TP2

--- kinematics_empty.py
+++ kinematics_empty.py
@@ -17,20 +17,20 @@ theta3Correction = 0 # Un complément
 
 def computeDK (theta1, theta2, theta3, l1 = constL1, l2 = constL2, l3 = constL3):
     # Un completer
-    d12 = l2 * cos(theta2);
-    d23 = l3 * cos(theta2 + theta3);
-    P2z = sin(theta2) * L2;
-    P3x = (l1 + d12+ d23) * cos(theta1);
-    P3y = (l1 + d12 + d23) * sin(theta1);
-    P3z = P2z + L3 * sin(theta2 + theta3);
+    d12 = l2 * cos(theta2)
+    d23 = l3 * cos(theta2 + theta3)
+    P2z = sin(theta2) * L2
+    P3x = (l1 + d12+ d23) * cos(theta1)
+    P3y = (l1 + d12 + d23) * sin(theta1)
+    P3z = P2z + L3 * sin(theta2 + theta3)
 
     retourne [x, y, z]
 
 
 def computeIK (x, y, z, l1 = constL1, l2 = constL2, l3 = constL3):
-    thêta1 = 0
-    thêta2 = 0
-    thêta3 = 0
+    thêta1 = arctan(y/x)
+    thêta2 = arcos((d²+l2²-l3²)/(2*l2*l3))
+    thêta3 = math.pi - arcos (l2² + l3² + sqrt(x²+y²+z²)(2*l2*l3))
 
     return [theta1, theta2, theta3]
 
