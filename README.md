4d0b6495651d10526abed33332e5b2cc80479f94
Anth0o0 <anthony.raffeneau@ynov.com>
2020-10-12 21:31:38 +0200

    rendu final

---
+++ README.md
@@ -0,0 +1,7 @@
+Here is the README.md, a file explaining the process we decided to follow during this work.
+
+In tp1, it was a question of studying direct kinematics, we knew the angles of the three engines and we had to determine the coordinates in x, y and z of the point at the end of the arm.
+
+The second tp was on the inverse kinematics, we knew the coordinates of the end of the arm and we had to determine the angles.
+
+
--- kinematics_empty.py
+++ kinematics_empty.py
@@ -15,31 +15,43 @@ theta3Correction = 0 # Un complément
 # constL3 = 0,250
 
 
-def computeDK (theta1, theta2, theta3, l1 = constL1, l2 = constL2, l3 = constL3):
+def computeDK (theta1, theta2, theta3, l1 = constL1, l2 = constL2, l3 = constL3):       #direct kinematic, tp1
     # Un completer
     d12 = l2 * cos(theta2)
     d23 = l3 * cos(theta2 + theta3)
     P2z = sin(theta2) * L2
-    P3x = (l1 + d12+ d23) * cos(theta1)
+    P3x = (l1 + d12 + d23) * cos(theta1)
     P3y = (l1 + d12 + d23) * sin(theta1)
     P3z = P2z + L3 * sin(theta2 + theta3)
 
     retourne [x, y, z]
 
 
-def computeIK (x, y, z, l1 = constL1, l2 = constL2, l3 = constL3):
+def computeIK (x, y, z, l1 = constL1, l2 = constL2, l3 = constL3):                      #inverse kinematic, tp2
     thêta1 = arctan(y/x)
-    thêta2 = arcos((d²+l2²-l3²)/(2*l2*l3))
+    thêta2 = arcos((d²+ l2²-l3²)/(2*l2*l3))
     thêta3 = math.pi - arcos (l2² + l3² + sqrt(x²+y²+z²)(2*l2*l3))
 
     return [theta1, theta2, theta3]
 
 
 def main ():
-    print ("Test des fonctions cinématiques ...")
+    print ("Test des fonctions cinématiques ...")                                       #we try to find out if it works 
     impression(
-        "computeDK (0, 0, 0) = {}". format (
-            computeDK (0, 0, 0, l1 = constL1, l2 = constL2, l3 = constL3)
+        "computeDK (x=287.7, y=0, z=0) = {}". format(
+            computeDK(math.radian(0), 0, 0, l1=constL1, l2=constL2, l3=constL3)
+        )
+    )
+
+    print(
+        "computeDk(90, 0, 0) = {} . expected x=0, y=287.7, z=0".format(
+            computeDK(math.radian(90), 0, 0, l1=constL1, l2=constL2, l3=constL3)
+        )
+    )
+
+    print(
+        "computeDk(0, 90, 0) = {} . expected x=0, y=287, z=156.7".format(
+            computeDK(0, math.radian(90), 0, l1=constL1, l2=constL2, l3=constL3)
         )
     )
 
